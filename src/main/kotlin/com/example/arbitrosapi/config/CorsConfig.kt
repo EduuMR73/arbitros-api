d67db6cc8b684877ff07488548d8f8d9a349f3a2
EduuMR73 <eduumr@gmail.com>
2025-11-21 19:54:37 +0100

    Permitir cualquier origen en CORS temporalmente

--- src/main/kotlin/com/example/arbitrosapi/config/CorsConfig.kt
+++ src/main/kotlin/com/example/arbitrosapi/config/CorsConfig.kt
@@ -1,2 +1,23 @@
 package com.example.arbitrosapi.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/api/**")
+                    .allowedOrigins("*") // <-- así permite cualquier origen temporalmente
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(false)
+
+            }
+        }
+    }
+}
